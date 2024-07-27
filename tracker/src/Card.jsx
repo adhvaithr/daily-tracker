66ed2445e559dd705874a144b49a31c31eefed46
Pratham  Hebbar <prathamhebbar2021@gmail.com>
2024-07-27 16:28:30 -0700

    react js initialization code

---
+++ tracker/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card() {
+    return (
+        <div className="card">
+            <img className="card-image" src="" alt="profile picture"></img>
+            <h2 className="card-title">{}</h2>
+            <p className="card-text">{}</p>
+        </div>
+    );
+}
+
+export default Card;
